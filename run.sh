fd64ed0e9ac0b2a630baa99e0c74b2300726d830
Sakib Haque Zisan <66489230+Sakib-Haque-Zisan@users.noreply.github.com>
2023-10-11 19:51:46 +0600

    Update run.sh

--- run.sh
+++ run.sh
@@ -34,7 +34,7 @@ if [ ! -f "$url/recon/final.txt" ];then
 	touch $url/recon/final.txt
 fi
  
-echo "[+] Harvesting subdomains with assetfinder..."
+echo "[+] Finding subdomains with assetfinder.. Thanks Zisan vai..."
 assetfinder $url >> $url/recon/assets.txt
 cat $url/recon/assets.txt | grep $1 >> $url/recon/final.txt
 rm $url/recon/assets.txt
@@ -44,7 +44,7 @@ rm $url/recon/assets.txt
 #sort -u $url/recon/f.txt >> $url/recon/final.txt
 #rm $url/recon/f.txt
  
-echo "[+] Probing for alive domains..."
+echo "[+] Searching for alive domains in EHP Course..."
 cat $url/recon/final.txt | sort -u | httprobe -s -p https:443 | sed 's/https\?:\/\///' | tr -d ':443' >> $url/recon/httprobe/a.txt
 sort -u $url/recon/httprobe/a.txt > $url/recon/httprobe/alive.txt
 rm $url/recon/httprobe/a.txt
